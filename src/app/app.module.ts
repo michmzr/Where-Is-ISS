d3971f23cb8665ed666996ea80393af11b0ac766
Michał Mazur <michal.mazur@truesoftware.pl>
2017-05-27 22:36:37 +0200

    - fixes packages - removed file with rxjs imports - removed file Server.js for express plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {MaterialModule} from "@angular/material";
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {AgmCoreModule} from "angular2-google-maps/core";
-import {environment} from "../environments/environment";
 
 import {AppComponent} from "./app.component";
 import {ISSPositionComponent} from "./iss/issposition.component";
-import "./shared/rxjs.imports";
+
+import {environment} from "../environments/environment";
 
 let gmapsApi = environment.api.gmaps;
 
